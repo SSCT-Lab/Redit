91f025553a619ae45f1621252c92691407ceaf37
Zheyuan Lin <lzy9986@126.com>
2022-01-20 19:31:10 +0800

    zookeeper check

--- redit/src/test/java/io/redit/TarGzDecompressTest.java
+++ redit/src/test/java/io/redit/TarGzDecompressTest.java
@@ -35,6 +35,7 @@ public class TarGzDecompressTest {
     }
     private static boolean deleteDir(File dir) {
         if (dir.isDirectory()) {
+
             String[] children = dir.list();
             for (int i=0; i<children.length; i++) {
                 boolean success = deleteDir(new File(dir, children[i]));
