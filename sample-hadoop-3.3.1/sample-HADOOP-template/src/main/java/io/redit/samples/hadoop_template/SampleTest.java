f5cbf6281de795f1b5fa870c101166748a569b0e
zhoumengbo <zhoumengbo1106@163.com>
2022-02-28 20:38:47 +0800

    add hadoop-3.3.1

---
+++ sample-hadoop-3.3.1/sample-HADOOP-template/src/main/java/io/redit/samples/hadoop_template/SampleTest.java
@@ -0,0 +1,42 @@
+package io.redit.samples.hadoop_template;
+
+import io.redit.ReditRunner;
+import io.redit.exceptions.RuntimeEngineException;
+import io.redit.execution.CommandResults;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+
+public class SampleTest {
+    private static final Logger logger = LoggerFactory.getLogger(SampleTest.class);
+    protected static ReditRunner runner;
+
+    @BeforeClass
+    public static void before() throws RuntimeEngineException, ParserConfigurationException, IOException, SAXException, TransformerException, InterruptedException {
+
+        runner = ReditRunner.run(ReditHelper.getDeployment());
+        ReditHelper.startNodesInOrder(runner);
+        ReditHelper.waitActive();
+        logger.info("The cluster is UP!");
+    }
+
+    @AfterClass
+    public static void after() {
+        if (runner != null) {
+            runner.stop();
+        }
+    }
+
+    @Test
+    public void sampleTest() throws RuntimeEngineException, InterruptedException {
+        ReditHelper.transitionToActive(1, runner);
+        ReditHelper.checkNNs(runner);
+    }
+
+}
